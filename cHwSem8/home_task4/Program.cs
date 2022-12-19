b2a3f79826680375a5a8cbc3f1be39cbd79acd43
Alexey <a.d.kamenev@yandex.ru>
2022-12-19 10:01:03 +0300

    4th task started

--- cHwSem8/home_task4/Program.cs
+++ cHwSem8/home_task4/Program.cs
@@ -2,7 +2,7 @@
 using MyMethod;
 
 MyGenerate gen = new MyGenerate();
-
+Random rnd = new Random();
 /*
 Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
@@ -12,3 +12,40 @@ MyGenerate gen = new MyGenerate();
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1)
 */
+
+int[,,] ArrayGenDI()
+    {
+        int rows = IntInput("число строк");
+        int columns = IntInput("число столбцов");
+        int depth = IntInput("глубина массива");
+        int min = 10;
+        int max = 99;
+        int element = rnd.Next(min,max);
+        int[,,] array = new int[rows, columns, depth];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j= 0; j < columns; j++){
+                for (int k= 0; k < depth; k++){
+                    while (!(unicumInArr(array, element))){
+                        element = rnd.Next(min,max);
+                    }
+                    array[i,j,k] = element;
+                }
+            }  
+        }
+        // отладка:
+        // my.Print(array);
+        return array;
+    }
+
+    bool unicumInArr (int[,,] usersArray, int num){
+        for (int i = 0; i < usersArray.GetLength(0); i++)
+        {
+            for (int j= 0; j < usersArray.GetLength(1); j++){
+                for (int k= 0; k < usersArray.GetLength(2); k++){
+                    if (usersArray[i,j,k] == num) return false;
+                }
+            }  
+        }
+        return true;
+    }
