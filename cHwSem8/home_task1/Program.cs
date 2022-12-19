c11df1a7d2f8fb3b56e95984de5b2ccbf504c401
Alexey <a.d.kamenev@yandex.ru>
2022-12-19 15:08:39 +0300

    1st task unstable version

--- cHwSem8/home_task1/Program.cs
+++ cHwSem8/home_task1/Program.cs
@@ -15,3 +15,27 @@ MyGenerate gen = new MyGenerate();
 9 5 3 2
 8 4 4 2
 */
+
+int[,] myArr = gen.ArrayGenDI();
+Print(myArr);
+
+int[,] sortByRaw = SortedArrayByRaw(myArr);
+Print(sortByRaw);
+
+int[,] SortedArrayByRaw (int[,] usersArray){
+    int[,] editedArray = new int [usersArray.GetLength(0),usersArray.GetLength(1)];
+    for (int i = 0; i < usersArray.GetLength(0); i++){
+        int temp = 0;
+        for (int j = 0; j < usersArray.GetLength(1)-1; j++){
+            if (usersArray[i,j] < usersArray[i,j+1]){
+                temp = usersArray[i,j];
+                usersArray[i,j] = usersArray[i,j+1];
+                usersArray[i,j+1] = temp;
+                j=0;
+            }
+        }
+        //Print(usersArray);
+    }
+
+    return usersArray;
+} 
